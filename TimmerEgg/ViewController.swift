4ea85d9a65d8acf8edba61a61a9defd4863b2170
Junior <wojunior90@outlook.com>
2020-07-08 11:42:45 -0300

    Initial Commit

---
+++ TimmerEgg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimmerEgg
+//
+//  Created by Wagner Ongaro Junior on 08/07/20.
+//  Copyright © 2020 Wagner Ongaro Junior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
